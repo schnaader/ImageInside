62a107df3f9c71e7f17c469ce234c2df7538c521
Christian Schneider <schnaader@gmx.de>
2021-07-18 17:31:19 +0200

    correlation coefficient computation

---
+++ Correlation.cpp
@@ -0,0 +1,34 @@
+#include <cmath>
+#include <cfloat>
+
+// Computes the correlation coefficient of two rows of image data x and y with a length of y bytes.
+// The correlation coefficient is defined as the result of dividing the empiric covariance by the empiric variance.
+
+static double CorrelationCoefficient(unsigned char* x, unsigned char* y, int length) {
+	// calculate means of x and y
+	double xSum = 0.0;
+	double ySum = 0.0;
+	for (int i = 0; i < length; i++) {
+		xSum += x[i];
+		ySum += y[i];
+	}
+	double xMean = xSum / length;
+	double yMean = ySum / length;
+	
+	double covariance = 0, varianceX = 0, varianceY = 0;
+	for (int i = 0; i < length; i++) {
+		double xDiff = x[i] - xMean;
+		double yDiff = y[i] - yMean;
+		
+		covariance += xDiff * yDiff;
+		varianceX += xDiff * xDiff;
+		varianceY += yDiff * yDiff;
+	}
+	
+	// prevent division by zero
+	if (varianceX * varianceY < DBL_EPSILON) {
+		return 0;
+	}
+	
+	return covariance / std::sqrt(varianceX * varianceY);
+}
